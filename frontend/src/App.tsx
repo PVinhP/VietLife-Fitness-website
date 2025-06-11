5c1a60c59d5bc85c0dd9fe578186846977a82a51
PVinhP <vinhpham753951@gmail.com>
2025-06-11 10:01:40 +0700

    update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,20 @@
-
-import './App.css'
+import './App.css' 
 import './App.css';
+import { BrowserRouter } from 'react-router-dom'; // Thêm import này
 import Allroutes from './routes/Allroutes';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+
 function App() {
   return (
-    <div className="App">
-    <Navbar/>
-
-
-
-    <Allroutes/>
-    <Footer/>
-
-
-    </div>
+    <BrowserRouter> {/* Wrap toàn bộ app với BrowserRouter */}
+      <div className="App">
+        <Navbar/>
+        <Allroutes/>
+        <Footer/>
+      </div>
+    </BrowserRouter>
   );
 }
 
-export default App;
+export default App; 
