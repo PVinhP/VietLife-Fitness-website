bd8d4b106cfd36d807db8207f582c5454939ef84
PVinhP <vinhpham753951@gmail.com>
2026-01-04 22:00:29 +0700

    host

--- frontend/src/pages/ForgotPassword.tsx
+++ frontend/src/pages/ForgotPassword.tsx
@@ -3,10 +3,10 @@ import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
+https://vietlife-fitness-website-host.onrender.com/
 const ForgotPassword: React.FC = () => {
   const navigate = useNavigate();
-  const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8080";
+  const API_URL = process.env.REACT_APP_API_URL || "https://vietlife-fitness-website-host.onrender.com";
 
   // Step 1: Nhập email, Step 2: Nhập OTP & Đổi mật khẩu
   const [step, setStep] = useState<1 | 2>(1);
